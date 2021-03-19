01e9f94938d6035754192914938407b02e4e9b56
仁志 <legend1world@gmail.com>
2021-03-19 20:46:58 +0900

    up

--- js/main.js
+++ js/main.js
@@ -1,5 +1,47 @@
 function ini(){
 
-		document.getElementById('main').innerHTML = '<iframe width="100%" height="1000px" src="home/index.html" frameborder="0" id="yout"></iframe>';
+	location.reload();
+	
+	//document.getElementById('main').innerHTML = '<iframe width="100%" height="1000px" src="home/index.html" frameborder="0" id="yout"></iframe>';
+
+}
+
+function rss(){
+	
+	$("#bs_chat").hide();
+	$("#rule").hide();
+	$("#manual").hide();
+	$("#question").hide();
+	$('#rss').fadeIn();
+	
+}
+
+function rule(){
+
+	$("#bs_chat").hide();
+	$("#rss").hide();
+	$("#manual").hide();
+	$("#question").hide();
+	$('#rule').fadeIn();
+
+}
+
+function manual(){
+
+	$("#bs_chat").hide();
+	$("#rss").hide();
+	$("#rule").hide();
+	$("#question").hide();
+	$('#manual').fadeIn();
+
+}
+
+function question(){
+
+	$("#bs_chat").hide();
+	$("#rss").hide();
+	$("#manual").hide();
+	$("#rule").hide();
+	$('#question').fadeIn();
 
 }
