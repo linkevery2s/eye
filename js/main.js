53f28e22025fa46c6d94dea3b15b0bb5a7ddd58e
仁志 <legend1world@gmail.com>
2021-03-20 17:24:57 +0900

    up

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ function ini(){
 	
 	//document.getElementById('main').innerHTML = '<iframe width="100%" height="1000px" src="home/index.html" frameborder="0" id="yout"></iframe>';
 
+
 }
 
 function rss(){
