ae14b80c7d7da1cfd9120dc1964e12a582b2f3bb
仁志 <legend1world@gmail.com>
2021-03-21 20:23:23 +0900

    up

--- js/main.js
+++ js/main.js
@@ -38,4 +38,33 @@ $("#question").hide();
 
 }
 
+/* 保留 */
+var zoom_channel;
+zoom_channel = 0;
+function zoom(){
+	if(zoom_channel == 0){
+	$('#header').fadeOut();
+	$('#footer').fadeOut();
+	$('#main').css('padding-top','10px');
+	$('#main').css('padding-bottom','0');
+	zoom_channel = 1;
+	}else{
+	$('#header').fadeIn();
+	$('#footer').fadeIn();
+	$('#main').css('padding-top','100px');
+	$('#main').css('padding-bottom','100px');
+	zoom_channel = 0;
+	}
+}
 
+function listen(){
+
+	$("#rss").hide();
+	$("#manual").hide();
+	$("#question").hide();
+	$("#practice").hide();
+
+	$('#bs_chat').fadeIn();
+	document.getElementById('bs_chat').innerHTML = '<iframe width="100%" height="500px" src="home/index.html" frameborder="0" id="yout"></iframe>';
+
+}
