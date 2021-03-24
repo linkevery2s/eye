fe4dd276bdbfd6027615ee42feff4f13aa47e1ff
仁志 <legend1world@gmail.com>
2021-03-24 19:14:20 +0900

    Update eye.js

--- js/eye.js
+++ js/eye.js
@@ -21,9 +21,6 @@ delay: 1000,
 	}).then(function(){
 	
 
-document.getElementById('sns').innerHTML = '<a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-size="large" data-text="EYE -防災アシスタント" data-url="https://bseye17.netlify.app/index.html" data-lang="ja" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script><div class="fb-like" data-href="https://bseye17.netlify.app/index.html" data-width="" data-layout="button_count" data-action="like" data-size="large" data-share="true"></div>';
-
-
 	});
 
   });
