e17f732c67929105c3093e91b0c86099650e84ec
仁志 <legend1world@gmail.com>
2021-12-26 13:49:25 +0900

    up

--- js/eye.js
+++ js/eye.js
@@ -13,6 +13,13 @@ const day = new Date();
 const jikan = day.getHours();
 const eyeui = new BotUI('bs_chat');
 
+// キーワード
+let key;
+
+// カウント数
+let i;
+let max_c;
+
 (function() {
 
   //挨拶文
@@ -33,22 +40,114 @@ const eyeui = new BotUI('bs_chat');
       content: 'こんばんは。'
     }).then(init);
 
+})(); /* function */
 
-  function init() {
-    eyeui.message.bot({
+function init() {
+  eyeui.message.bot({
+    delay: 1000,
+    content: '今日は何を学びますか？'
+  }).then(function() {
+
+    data_catch();
+
+    /*eyeui.message.bot({
       delay: 1000,
-      content: '今日は何を学びますか？'
-    }).then(function() {
-      
-        image_data.innerHTML = "a";
-        $('.link_intro').fadeIn();
-      /*eyeui.message.bot({
-      	delay: 1000,
-      	content: '知りたいことを色々と聞いてくださいね。'
-      }).then(function(){
-      });*/
+      content: '知りたいことを色々と聞いてくださいね。'
+    }).then(function(){
+    });*/
+
+  });
+} /*init()*/
+
+
+function data_catch() {
+
+  // データ取得
+  fetch('https://bseye17.netlify.app/data/output.json')
+    .then(response => response.json())
+    .then(data => {
+
+      // 最大数を取得
+      max_c = data.length;
+
+
+      return eyeui.action.button({
+        delay: 500,
+        action: [{
+            text: data[0].sub,
+            value: "0"
+          },
+          {
+            text: data[1].sub,
+            value: "1"
+          },
+          {
+            text: data[2].sub,
+            value: "2"
+          },
+          {
+            text: data[3].sub,
+            value: "3"
+          },
+          {
+            text: "その他",
+            value: "10000"
+          },
+        ]
+
+      }).then(function(res) {
+
+        key = res.value;
+
+        eyeui.message.bot({
+          delay: 1000,
+          content: data[key].contents
+        }).then(function() {
+          // 参考写真があったら
+          if (!(data[key].image == "")) {
+
+            eyeui.message.bot({
+              delay: 1000,
+              content: "参考画像を添付します。"
+            }).then(function() {
+              image_data.innerHTML = '<a href="images/post/' + data[3].image + '" class="intro_photo" data-lightbox="image" data-title="参考画像">参考画像</a>';
+              $('.link_intro').fadeIn();
+              tugi();
+            });
+
+          } else {
+            tugi();
+          }
+
+        });
+
+      }); /* res */
+
     });
-  }/*init()*/
 
+} /* data_catch() */
 
-})();/* function */
+/* 質問をループさせる */
+function tugi() {
+
+  eyeui.message.bot({
+    delay: 1500,
+    content: '他に知りたいことはありますか？'
+  }).then(function() {
+    return eyeui.action.button({
+      delay: 1000,
+      action: [{
+        icon: 'circle-thin',
+        text: 'はい',
+        value: true
+      }, {
+        icon: 'close',
+        text: 'いいえ',
+        value: false
+      }]
+    });
+  }).then(function(res) {
+    res.value ? init() : end();
+  });
+
+}
